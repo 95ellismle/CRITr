3540dbd91e400ebdd0b58a240abc747d149a3edc
Matt <95ellismle@gmail.com>
2020-01-13 20:10:55 +0000

    Fixed small bug, the nginx part of the docker-compose seems to work now. Will test on actual server tomorrow

--- CRITr/CRITr/settings.py
+++ CRITr/CRITr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ['web']
+ALLOWED_HOSTS = ['web', "maps.critr.org.uk"]
 
 
 # Application definition
